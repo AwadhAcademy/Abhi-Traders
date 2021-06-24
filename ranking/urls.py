dc0100444e0797d771b74bcba04e5a7fca5072d9
AwadhAcademy <feelsconceptwithawadh>
2021-06-24 13:54:21 -0700

    abhi12

--- ranking/urls.py
+++ ranking/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from djano.views.settings import serve
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
